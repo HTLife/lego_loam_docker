05eafaf3c034a5376f0c48b4b3148095aa7ab4ad
JackyLiu <jjkka132@gmail.com>
2020-08-11 13:00:13 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ xhost +
 ## Testing if OpenGL works
 # docker run --runtime=nvidia --rm -it --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" legoloam glxgears
 
-docker run --runtime=nvidia --rm -it --name legoloam --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $HOME/docker_share:/home jjkka132/lego_loam_docker:latest  
+docker run  --gpus all --rm -it --name legoloam --env="DISPLAY" --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v $HOME/docker_share:/home jjkka132/lego_loam_docker:latest  
